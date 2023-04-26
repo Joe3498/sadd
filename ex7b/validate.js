ee59940a7feec4e14411086ab3c28d1fa97e4e7a
Jaysophz <124852179+Jaysophz@users.noreply.github.com>
2023-04-26 19:14:02 +0700

    Add files via upload

---
+++ ex7b/validate.js
@@ -0,0 +1,44 @@
+function checkSID() {
+  let sid = (document.getElementById("sid").value).trim();
+
+  if (isNaN(sid))
+  {
+	return false;
+  }
+  
+	if (sid.length == 10 ) 
+	{
+		return true;
+	} 
+	  else 
+	  {
+		return false;
+	  }
+
+}
+
+function checkCandiNo() {
+  let candi = (document.getElementById("candi").value).trim();
+  if (isNaN(candi) || candi < 1 || candi > 10 ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validateForm(){
+	if(!checkSID()){
+	  alert("Invalid value for Student ID!");
+	  document.getElementById("sid").focus();
+	  return false;
+	}else{
+		if(!checkCandiNo()){
+		  alert("Invalid value for Candidate No!!");
+		  document.getElementById("candi").focus();
+		  return false;
+		}else{
+			alert("Your input data passes validation!!");
+			return true;
+		}
+	}
+}
